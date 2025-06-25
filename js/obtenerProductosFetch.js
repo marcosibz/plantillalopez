708c47b3be49f6b3cdbf060210e39b416a52fc5b
marcosibz <ibanez.marcos@tecnica7.edu.ar>
2025-06-25 20:25:38 -0300

    funciones asincronicas o diferentes

--- js/obtenerProductosFetch.js
+++ js/obtenerProductosFetch.js
@@ -1,16 +1,40 @@
-fetch("htps://fakestoreapi/products")
-.then(response => response.json())
-.then(json => console.log(json))
+fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
 
 const endpoint = '/data/datos.json';
 
 
 // funcion asincronica
 const obtenerProd = async() => {
+    response = await fetch(endpoint);
+    response = response.json();
+    console.log(response);
     
-response = await fetch(endpoint);
-response = await response.json();
-console.log(response);
+    obtenerProd();
 }
 
+// funcion asincronica
+const obtenerProd2 = async() => {
+    
+response = await fetch(endpoint);
+response = response.json();
+console.log(response);
+ catch(error) {
+    console.log("error al leer los datos")
+}
+}
 obtenerProd();
+
+
+// funcion asincronica
+const obtenerProd3 = async() => {
+    try{
+    response = await fetch(endpoint);
+    response = response.json();
+    console.log(response);
+    } catch(error) {
+        console.log("error al leer los datos")
+    }
+    }
+    obtenerProd();
