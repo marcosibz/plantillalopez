11e2c0d2207a25cbab81b1828576184c06cd1b8a
marcosibz <ibanez.marcos@tecnica7.edu.ar>
2025-07-02 20:28:38 -0300

    agregar divs mediante js

--- js/tienda.js
+++ js/tienda.js
@@ -14,12 +14,31 @@ document.addEventListener("DOMContentLoaded", () => {
     getProductos();
 
 
-    const contenedorProd = document.querySelector('Selection.productos')
-    const mostrarProductos = (datos) => {
-        datos:foreach(item => {
+    const contenedorProd = document.querySelector('section.productos')
+    const mostrarProductos = datos => {
+        datos.forEach(item => {
             const card = document.createElement('div')
             card.className = 'card'
-            card.innerHTML = ` `
+            card.innerHTML = ` <div class="top">
+                        <img src="images/productos/kiwis.jpg" alt="">
+                        <p class="titProd">PRODUCTO 5</p>
+                        <p class="descrpcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                        <a href="opiniones.html" class="resenias">ver reseñas</a>
+                    </div>
+                    <div class="bottom">
+                        <div class="precio">
+                            <span>$1150</span> <button class="favorito"><i
+                                    class="fa-solid fa-heart"></i></button></span>
+                        </div>
+                        <div class="agregar">
+                            <div class="amount">
+                                <span class="descrpcion"> Cant </span>
+                                <input type="number" name="cant" id="cant" min="0" value="1">
+                            </div>
+                            <button class="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
+                        </div>
+                    </div>
+                `
             contenedorProd.appendChild(card)
         });
     }
