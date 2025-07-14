1f478562acb20b84b6d34f79f44280b1edbc5274
marcosibz <ibanez.marcos@tecnica7.edu.ar>
2025-07-14 20:24:43 -0300

    ahora los productos que se agregan al carrito se guardan en el local storage junto con sus datos como la cantidad

--- js/carritoModulos.js
+++ js/carritoModulos.js
@@ -1,7 +1,8 @@
-    const agregarAlCarrito = (item) => {
-    let = carrito = JSON.parse(localStorage.getItem("carrito")) || []
-    carrito.push(item)
-    // JSON.stringify convierte los datos a formato json
-    localStorage.setItem("carrito",JSON.stringify(item))
-    alert('item agregado')
+const agregarAlCarrito = (item, cant) => {
+let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+console.log(carrito)
+item.amount = cant;
+carrito.push(item)
+localStorage.setItem("carrito", JSON.stringify(carrito))
+alert('producto agregado')
 }
--- js/tienda.js
+++ js/tienda.js
@@ -1,26 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const endpoint = "/data/datos.json";
 
-    const endpoint = '/data/datos.json';
-
-    const getProductos = async () =>{
-        try { response = await fetch(endpoint) 
-            datos = await response.json()
-            console.log(datos)
-            mostrarProductos(datos)
-        } catch(error) {
-            console.log("error");
-        }
+  const getProductos = async () => {
+    try {
+      response = await fetch(endpoint);
+      datos = await response.json();
+      console.log(datos);
+      mostrarProductos(datos);
+    } catch (error) {
+      console.log("error");
     }
-    getProductos();
+  };
+  getProductos();
 
-
-    const contenedorProd = document.querySelector('section.productos')
-    const mostrarProductos = datos => {
-        datos.forEach(item => {
-            const card = document.createElement('div')
-            card.className = 'card'
-            card.innerHTML = ` <div class="top">
-                        <img src="images/productos/kiwis.jpg" alt="">
+  const contenedorProd = document.querySelector("section.productos");
+  const mostrarProductos = (datos) => {
+    datos.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = ` <div class="top">
+                        <img src="images/productos/${item.image}" alt="">
                         <p class="titProd">PRODUCTO 5</p>
                         <p class="descrpcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                         <a href="opiniones.html" class="resenias">ver reseñas</a>
@@ -35,18 +34,17 @@ document.addEventListener("DOMContentLoaded", () => {
                                 <span class="descrpcion"> Cant </span>
                                 <input type="number" name="cant" id="cant" min="0" value="1">
                             </div>
-                            <button class="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
+                            <button class="addcarrito" id="addcarrito"><i class="fa-solid fa-cart-plus"></i></button>
                         </div>
                     </div>
-                `
-            contenedorProd.appendChild(card)
-        });
-    }
+                `;
+      contenedorProd.appendChild(card);
 
-})
-
+      card.querySelector("#addcarrito").onclick = () => {
+        const cant = parseInt(card.querySelector('[name = "cant"]').value);
+        agregarAlCarrito(item, cant);
+      };
+    });
+  };
+});
 // evento btn agregar
-
-card.querySelector("#addcarrito").onclick = () => {
-    agregarAlCarrito(item)
-}
